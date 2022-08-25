b5a105da91d41931642eb3354303cd0369c6dd96
Grace Alfonsi <cunniff.grace@gmail.com>
2022-08-25 07:36:48 -0500

    initial commit

---
+++ js/homework.js
@@ -0,0 +1,25 @@
+// // Exercise 1 - Closures
+// Update the createAdder function below so that
+// the below code works as intended
+
+function createAdder(x){
+    function add(y){
+        return x + y
+    }
+    return add
+
+};
+
+const addEight = createAdder(8);
+console.log(addEight(10)); // 18
+console.log(addEight(17)); // 25
+console.log(addEight(50)); // 58
+console.log(addEight(100)); // 108
+console.log(addEight(92)); // 100
+
+const addThree = createAdder(3);
+console.log(addThree(10)); // 13
+console.log(addThree(17)); // 20
+console.log(addThree(50)); // 53
+console.log(addThree(100)); // 103
+console.log(addThree(92)); // 95
